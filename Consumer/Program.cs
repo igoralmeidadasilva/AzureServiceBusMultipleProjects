939b0586ddda6c280a8faec24cfa7fb59778afad
Igor <Igor.almeidadasilva13@gmail.com>
2023-10-02 20:45:22 -0300

    Initial Commit

---
+++ Consumer/Program.cs
@@ -0,0 +1,50 @@
+﻿using Azure.Messaging.ServiceBus;
+
+namespace Consumer
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ServiceBusClient client;
+            ServiceBusProcessor processor;
+            const string? connectionString = "";
+            const string? queueName = "";
+
+            client = new ServiceBusClient(connectionString);
+            processor = client.CreateProcessor(queueName);
+
+            try
+            {
+                processor.ProcessMessageAsync += MessageHandler;
+                processor.ProcessErrorAsync += ErrorHandler;
+
+                await processor.StartProcessingAsync();
+                Console.WriteLine("Ouvindo...");
+                Console.WriteLine("Pressione uma tecla para parar.");
+                Console.ReadKey();
+                await processor.StopProcessingAsync();
+
+            }
+            finally
+            {
+                await processor.DisposeAsync();
+                await client.DisposeAsync();
+            }
+
+        }
+        static async Task MessageHandler(ProcessMessageEventArgs args)
+        {
+            string body = args.Message.Body.ToString();
+            Console.WriteLine($"Received: {body}");
+
+            await args.CompleteMessageAsync(args.Message);
+        }
+
+        static Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,47 @@
+﻿using Azure.Messaging.ServiceBus;
+using Azure.Identity;
+
+namespace Producer
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ServiceBusClient client;
+            ServiceBusSender sender;
+            const string? connectionString = "";
+            const string? queueName = "";
+            const int numMessage = 3;
+
+            client = new ServiceBusClient(connectionString);
+            sender = client.CreateSender(queueName);
+
+            using ServiceBusMessageBatch batch = await sender.CreateMessageBatchAsync();
+
+            for (int i = 0; i <= numMessage; i++)
+            {
+                string message = $"Mensagem: {i+1} da fila.";
+                if (!batch.TryAddMessage(new ServiceBusMessage(message))) 
+                {
+                    Console.WriteLine("Um erro aconteceu.");
+                    throw new Exception("Erro ao inserir a mensagem");
+                }
+            }
+
+            try
+            {
+                Console.WriteLine("Pressione uma tecla para enviar as mensagens.");
+                Console.ReadKey();
+                await sender.SendMessagesAsync(batch);
+                Console.WriteLine("Pressione uma tecla para parar.");
+                Console.ReadKey();
+            }
+            finally
+            {
+                Console.WriteLine($"{numMessage} foram enviadas para a fila");
+                await sender.DisposeAsync();
+                await client.DisposeAsync();
+            }
+        }
+    }
+}
